552eed028379bc4ed6cb9d45b328bc36983d990c
Akshath Jalda <48559696+akjalda@users.noreply.github.com>
2019-10-09 22:11:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+//declaring variables
+var input = document.getElementById("inputx");
+var btn = document.getElementById("btnx");
+var list = document.getElementById("listx");
+//adding button and input eventlisteners
+btn.addEventListener("click", addItems);
+input.addEventListener("keypress", addItems);
+//button listener function
+function addItems() {
+    if (input.value == "") {
+        alert("enter something...");
+    }
+    else {
+        var element = document.createElement("li");
+        element.appendChild(document.createTextNode(input.value));
+        list.appendChild(element);
+        var new_btn = document.createElement("button");
+        new_btn.appendChild(document.createTextNode("remove"));
+        list.appendChild(new_btn);
+        input.value = "";
+        new_btn.style.color = "red";
+        new_btn.style.borderColor = "black";
+        new_btn.style.fontSize = "16px";
+        new_btn.style.borderRadius = "15px";
+        new_btn.addEventListener("click", removeItem);
+        function removeItem() {
+            element.style.display = "none";
+            new_btn.style.display = "none";
+        }
+    }
+}
+//input listener function
+function addItems(event) {
+    if (event.keyCode == 13) {
+        if (input.value == "") {
+            alert("enter something...");
+        }
+        else {
+            var element = document.createElement("li");
+            element.appendChild(document.createTextNode(input.value));
+            list.appendChild(element);
+            var new_btn = document.createElement("button");
+            new_btn.appendChild(document.createTextNode("remove"));
+            list.appendChild(new_btn);
+            input.value = "";
+            new_btn.style.color = "red";
+            new_btn.style.borderColor = "black";
+            new_btn.style.fontSize = "16px";
+            new_btn.style.borderRadius = "15px";
+            new_btn.addEventListener("click", removeItem);
+            function removeItem() {
+                element.style.display = "none";
+                new_btn.style.display = "none";
+            }
+        }
+    }
+}
+
